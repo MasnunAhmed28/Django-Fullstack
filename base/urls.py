b94589e5ce1e15afe61a571b8067e9f931037733
MasnunAhmed <masnunahmed19@gmail.com>
2023-07-15 00:19:43 +0600

    all file commited

---
+++ base/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('login/',views.loginPage,name='login'),
+    path('register/',views.registerPage,name='register'),
+    path('logout/',views.logoutUser,name='logOut'),
+    path('',views.home,name='home'),
+    path('room/<str:pk>/',views.room,name='room'),
+    path('profile/<str:pk>/',views.userProfile,name='user-profile'),
+    path('create-room/',views.createRoom,name='create-room'),
+    path('update-room/<str:pk>/',views.updateRoom,name='update-room'),
+    path('delete-room/<str:pk>/',views.deleteRoom,name='delete-room'),
+    path('delete-message/<str:pk>/',views.deleteMessage,name='delete-message'),
+    path('update-user/',views.updateUser,name='update-user'),
+    path('topic/',views.topiclist,name='topic'),
+    path('activity/',views.activityside,name='activity'),
+
+]
