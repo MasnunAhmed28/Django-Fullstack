c7c57b87af62126f41376688a857a155dfcc248c
MasnunAhmed28 <139568010+MasnunAhmed28@users.noreply.github.com>
2024-01-22 22:33:35 +0600

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-)(j!p@sxx58n9(xv832hmd6gy4g_avr_o5ln4h+y2&dq_@v10%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -83,14 +84,11 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'a2Beb4fc*F11f*6fbF22BBdf1bB3aAba',
-        'HOST': 'viaduct.proxy.rlwy.net',
-        'PORT': '46030',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -126,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_ROOT=BASE_DIR/ 'static/images'
